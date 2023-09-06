60243c5f040e2700750ef88945c53b3d0d2081dc
Daniel <McsColosPlay@gmail.com>
2023-09-06 16:57:37 +0300

    Itinial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__btn_next',
+      prevEl: '.slider__btn_prev',
+      disabledClass: 'slider__btn_disabled'
+
+    },
+
+  });
+  
